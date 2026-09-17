ffc22790cc098f2b58bdd2aa30c1e0617200e9c6
Ender <164372602+EnderSlayer121@users.noreply.github.com>
2026-09-17 22:14:31 +0000

    horsegame random # broken

--- horsegame.cpp
+++ horsegame.cpp
@@ -8,5 +8,32 @@ bool isWinner(int horseNum, int* horses);
 
 int main(){
   int horses[5] = {0,0,0,0,0};
-  bool keepGoing = true
+  bool keepGoing = true;
+  std::string getOut;
+
+  while(keepGoing){
+    std::cout << "Entered while loop" << std::endl;
+    for(int horseNum = 0; horseNum <= 4; horseNum++){
+      std::cout << "Entered for loop" << std::endl;
+      advance(horseNum, &horses[horseNum]);
+      std::cout << horseNum << " " << horses[horseNum] << std::endl;
+      /*
+      printLane(horseNum, horses[horseNum]);
+      */
+    }// end for loop
+    std::cout << " Press Enter to continue" << std::endl;
+    std::getline(std::cin, getOut);
+    if (getOut.empty()){
+      keepGoing = true;
+    } else{
+      keepGoing = false;
+    }//end if
+  }// end while loop
 }// end main
+
+void advance(int horseNum, int* horses){
+  srand(time(NULL));
+  int coin = rand() % 2;
+  std::cout << coin << " is the Coin output" << std::endl;
+  horses[horseNum] += coin;
+}// end advance
