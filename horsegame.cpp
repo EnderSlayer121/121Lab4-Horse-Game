9ccda3ddff8f7727ee605aeb752a5c0ec4b51518
Ender <164372602+EnderSlayer121@users.noreply.github.com>
2026-09-18 18:48:17 +0000

    Fixed errors in horsegame

--- horsegame.cpp
+++ horsegame.cpp
@@ -7,15 +7,15 @@ void printLane(int horseNum, int* horses);
 bool isWinner(int horseNum, int* horses);
 
 int main(){
-  int horses[5] = {0,0,0,0,0};
+  int horses[5] = {0, 0, 0, 0, 0};
   bool keepGoing = true;
   std::string getOut;
-
+  srand(time(NULL));
   while(keepGoing){
     std::cout << "Entered while loop" << std::endl;
     for(int horseNum = 0; horseNum <= 4; horseNum++){
-      std::cout << "Entered for loop" << std::endl;
-      advance(horseNum, &horses[horseNum]);
+      std::cout << "Entered for loop. horseNum = " << horseNum << " horses[horseNum] = " << horses[horseNum] << std::endl;
+      advance(horseNum, horses);
       std::cout << horseNum << " " << horses[horseNum] << std::endl;
       /*
       printLane(horseNum, horses[horseNum]);
@@ -32,8 +32,7 @@ int main(){
 }// end main
 
 void advance(int horseNum, int* horses){
-  srand(time(NULL));
   int coin = rand() % 2;
-  std::cout << coin << " is the Coin output" << std::endl;
   horses[horseNum] += coin;
+  std::cout << coin << " is the Coin output. horses[horseNum] is " << horses[horseNum] << " horseNum is " << horseNum << std::endl;
 }// end advance
